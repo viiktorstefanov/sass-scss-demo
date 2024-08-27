9817fa1b38458d962cb44f3518bf5e5221d18f60
Viktor Stefanov <victorystefanov@gmail.com>
2024-08-27 20:15:48 +0300

    adding fonts reset colors and breakpoints

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
-import './styles/App.sass';
+import React from "react";
+import "./styles/App.scss";
 
 function App() {
   return (
     <div className="App">
-    
+      <div className="sidebar">
+        <h1>Header 1</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis,
+          beatae perferendis nisi porro odit vitae!
+        </p>
+      </div>
+      <div className="aside">
+        <div className="aside-container">
+          <h2>header 2</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nisi
+            veniam, perspiciatis fuga at qui minus culpa deleniti commodi iusto
+            debitis dolor voluptatum, saepe similique alias sed facilis
+            accusantium eligendi?
+          </p>
+        </div>
+        <div className="aside-container">
+          <h2>header 2</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nisi
+            veniam, perspiciatis fuga at qui minus culpa deleniti commodi iusto
+            debitis dolor.
+          </p>
+        </div>
+        <div className="aside-container">
+          <h2>header 2</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
+            deserunt reprehenderit magni quidem tempore molestias sit, earum
+            ratione!
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
